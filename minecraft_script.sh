6af0e10cb25cba98b2c6c62d9f7225c4230df22c
Jaydeep Hasmukhlal <jaydeeph.vaja@gmail.com>
2019-07-15 22:56:16 +0100

    Fixed minor typo.

--- minecraft_script.sh
+++ minecraft_script.sh
@@ -27,7 +27,7 @@ do
                 echo -e "${GREEN} Finished gettings updates. ${DEFAULT}"
                 ;;
             2)
-                udo cp ./minecraft.service /etc/systemd/system/
+                sudo cp ./minecraft.service /etc/systemd/system/
                 sudo apt-get install default-jre -y
                 curl -O https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar
                 mkdir minecraft
